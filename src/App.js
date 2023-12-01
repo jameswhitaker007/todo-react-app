ddd351abb67f57f79f907588451b6506fd894dfd
James Whitaker <jwhitaker0007@gmail.com>
2023-11-30 23:02:16 -0500

    Textbox added for todo user entry

--- src/App.js
+++ src/App.js
@@ -2,22 +2,42 @@ import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
 import Todos from "./Todos";
+import { useEffect } from "react";
 
 function App() {
   const [todos, setTodos] = useState([]);
   const [key, setKey] = useState(1);
 
-  function add() {
-    var name = "Todo " + key; //Item 1, Item 2, ...
+  useEffect(() => {
+    document.getElementById("todoInput").focus();
+  }, []);
+
+  function add(e) {
+    e.preventDefault();
+    //var name = "Todo " + key; //Item 1, Item 2, ...
+    var name = document.getElementById("todoInput").value;
+    console.log(name);
+    if ((name == "" || null)) {
+      return;
+    }
     setKey(key + 1); // Obtaining the next key associated with the todo item
-    todos.push({name, key});
+    todos.push({ name, key });
     setTodos([...todos]);
     //console.log(todos);
   }
 
   return (
     <>
-      <button onClick={add}>Add Todo</button>
+      <form>
+        <input
+          type="text"
+          name="todoInput"
+          id="todoInput"
+          placeholder="Add your todo"
+        />
+        <button onClick={add}>Add Todo</button>
+      </form>
+
       <Todos todos={todos} setTodos={setTodos} />
     </>
   );
